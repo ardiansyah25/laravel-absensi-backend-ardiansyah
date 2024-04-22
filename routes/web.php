70484a90f43cb8b4c8319dee109f38c38073f842
Ardiansyah <aardi25@GMAIL.COM>
2024-04-22 16:52:35 +0700

    fortify login

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
   return view('pages/dashboard', ['type_menu' => 'dashboard']);
 });
+
+Route::middleware(['auth'])->group(function () {
+  Route::get('home', function () {
+    return view('pages.dashboard', ['type_menu' => 'home']);
+  })->name('home');
+
+  // Route::resource('users', UserController::class);
+});
